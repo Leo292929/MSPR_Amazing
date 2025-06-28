58d65323c9157dec47bab48ce0317bbbf672d080
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2025-06-28 23:25:38 +0200

    a

--- fit_minibatch_kmeans.py
+++ fit_minibatch_kmeans.py
@@ -5,12 +5,12 @@ from load_client_data import stream_user_metrics_by_id_range
 from load_client_data import build_preprocessor_from_sample
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from tqdm import tqdm  # Progress bar
+from tqdm import tqdm  
 
 # Paramètres du clustering
 n_clusters = 4                                                          # Nombre de clusters souhaités
-batch_size = 1000                                                       # Taille des mini-batchs
-max_id = 10000                                                          # Dernier ID utilisateur à traiter
+batch_size = 10000                                                       # Taille des mini-batchs
+max_id = 15639775                                                          # Dernier ID utilisateur à traiter
 preprocessor = build_preprocessor_from_sample(sample_size=100000)       # Création du préprocessor
 
 # Initialisation du modèle MiniBatchKMeans
@@ -32,7 +32,7 @@ joblib.dump(kmeans, 'mini_batch_kmeans_model.pkl')
 print("Modèle MiniBatchKMeans sauvegardé sous 'mini_batch_kmeans_model.pkl'")
 
 
-X = stream_user_metrics_by_id_range(preprocessor, start_id=0, end_id=1000)
+X = stream_user_metrics_by_id_range(preprocessor, start_id=0, end_id=2000)
 kmeans_labels = kmeans.predict(X)
 
 # Réduction de dimension pour visualisation
